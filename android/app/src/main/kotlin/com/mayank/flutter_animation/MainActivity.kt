c6a7c17b73ba96a12dd40af2fc0b650d4a67870b
mktece <70434094+mktece@users.noreply.github.com>
2021-05-28 20:10:41 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/mayank/flutter_animation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mayank.flutter_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_animation/pages/splash_screen.dart';
+
+void main() => runApp(
+    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashPage()
+    )
+);
+
